531ca4078cd9572b3d68b8ec792a19968ef0b4aa
DeepInThought <support@deepinthought.io>
2018-10-06 05:36:02 -0500

    Fixing import errors

--- main.go
+++ main.go
@@ -1,14 +1,36 @@
 package main
 
 import (
-	_ "golang.org/x/sys/windows"
-	"runtime"
 	"fmt"
+	"os"
+	"runtime"
 	"time"
+	"log"
+
+	"golang.org/x/sys/windows"
 )
+
+// MainLogs Record logs and send to os.Stderr
+var MainLogs= mainLogs{}
+type mainLogs struct {
+	info, warn, err *log.Logger
+}
 
 func init() {
 	runtime.LockOSThread()
-	currentTime := time.Now()
-	fmt.Printf("%+v\n", currentTime)
+	MainLogs.info = log.New(os.Stderr, "INFO: ", log.Ltime)
+	MainLogs.warn = log.New(os.Stderr, "WARNING: ", log.Ltime)
+	MainLogs.err = log.New(os.Stderr, "ERROR: ", log.Ltime)
+	return
+}
+
+func main() {
+	deepTime := time.Now()
+	fmt.Println("INITIATING:")
+	fmt.Println("TIME: ", deepTime)
+}
+
+func version()  {
+	windows.
+	return
 }
